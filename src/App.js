e2698d4b596398760fd673dac310205bce6863e2
Alex Simpson <alxsimpson@gmail.com>
2020-07-04 13:12:07 +1000

    Build begin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>alexSimpson..</h1>
+        <ul>
+          <li class="active"><a>projects</a></li>
+          <li><a>about</a></li>
+          <li><a>contact</a></li>
+        </ul>
       </header>
+      <socials>
+          <div></div>
+          <div></div>
+          <div></div>
+      </socials>
+      <main>
+        <div class="preview active"></div>
+        <div class="small preview">        
+          <div class="preview" id="top"></div>
+          <div class="preview"></div>
+        </div>
+        <p>this is the main</p>
+      </main>
+
+      <footer>this is the footer
+
+      </footer>
     </div>
   );
 }
